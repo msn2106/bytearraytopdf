e584cde5eff8221920e484a17dd2b0584b024d34
msingh <msingh@cogitate.us>
2023-01-29 13:39:16 +0530

    Updates

---
+++ precache-manifest.22d0dafee16863cfe369cda66172f1ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac2de73ebd511cb87146fd4f59c4900",
+    "url": "/bytearraytopdf/index.html"
+  },
+  {
+    "revision": "b3c2514c4d82289b7c57",
+    "url": "/bytearraytopdf/static/css/main.d6342e39.chunk.css"
+  },
+  {
+    "revision": "1ab10477ecb9702e8302",
+    "url": "/bytearraytopdf/static/js/2.83615dc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bytearraytopdf/static/js/2.83615dc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c2514c4d82289b7c57",
+    "url": "/bytearraytopdf/static/js/main.63c88c38.chunk.js"
+  },
+  {
+    "revision": "6e0f4bfc7456a9c4c02e",
+    "url": "/bytearraytopdf/static/js/runtime-main.e21e8933.js"
+  },
+  {
+    "revision": "1cd294a771f26752bbb8d8d5210f6412",
+    "url": "/bytearraytopdf/static/media/Nunito-Bold.1cd294a7.ttf"
+  },
+  {
+    "revision": "d2e691bc4a2b696929172cb3d22ce8ba",
+    "url": "/bytearraytopdf/static/media/Nunito-Regular.d2e691bc.ttf"
+  }
+]);
